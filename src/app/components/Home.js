33b504d80433ef1c3550dcf90765d487a4d46f02
Shikha Gupta <Shikha@apples-MacBook-Pro-4.local>
2017-05-27 22:36:34 +0530

    Passing data using props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,11 +1,25 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 export class Home extends React.Component{
   render(){
-    return(
+      console.log(this.props);
+      return(
         <div className="container">
-            <p>Home content</p>
+            <h2>User Info</h2>
+            <p>Name:{this.props.name}</p>
+            <p>Age:{this.props.age}</p>
+            <h3>Extra Info</h3>
+            <p>Id:{this.props.extra.id}</p>
+            <p>Hobbies:</p>
+            <ul>
+                {this.props.extra.hobbies.map((hobby,i)=> <li key={i}>{hobby}</li>)}
+            </ul>
+            {this.props.children}
         </div>
     );
-  };
-};
+  }
+}
+
+
+
--- src/app/index.js
+++ src/app/index.js
@@ -6,11 +6,19 @@ import { Home } from "./components/Home";
 class App extends React.Component
 {
     render(){
+        var extra={
+            id: "xyz",
+            hobbies:["Reading","Writing"]
+        };
         return(
             <div>
                 <Header/>
                 <div className="container">
-                    <Home/>
+                    <Home name={"react"} age={25} extra={extra}>
+                        <h1>child 1 of home</h1>
+                        <p>child 2 of home</p>
+
+                    </Home>
                 </div>
 
             </div>
